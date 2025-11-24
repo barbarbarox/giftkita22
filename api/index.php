1780f88e6daea5fee86f1818fb7b3f9c5fdfe408
abarox <maulanaakbar1387@gmail.com>
2025-11-24 11:42:26 +0700

    ytta

---
+++ api/index.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+
+// Vercel forwards all requests to this file
+require __DIR__ . '/../public/index.php';
