8d56320e058ba31a3de0eb5ffb46ed20bda94815
abarox <maulanaakbar1387@gmail.com>
2025-11-24 12:23:05 +0700

    Fix Vercel PHP runtime configuration

--- api/index.php
+++ api/index.php
@@ -1,11 +1,4 @@
 <?php
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
-
-// Vercel forwards all requests to this file
+// Load Laravel
 require __DIR__ . '/../public/index.php';
